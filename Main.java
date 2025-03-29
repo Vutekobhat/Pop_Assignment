2ed78cdd0e3363c0a61e5df11661083654ef6e6d
Vutekobhat <grishmagar00@gmail.com>
2025-03-29 07:56:06 +0545

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
+
 package userInterface;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) {
-        // Suppress macOS-specific JavaFX logs (optional)
-        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "ExpenseManagement");
-
-        Login login = new Login();
-        try {
-            login.start(primaryStage);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Database.initializeDatabase();
+        Login login = new Login(primaryStage);
+        login.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
